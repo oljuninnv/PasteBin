96f6b1deb554c2b5030e06461d72a08c88ba7e9a
Олюнин Никита <5608nik@mail.ru>
2024-12-13 16:03:57 +0700

    feat: поменял языки в config'ах

--- paste_bin/config/voyager.php
+++ paste_bin/config/voyager.php
@@ -104,7 +104,7 @@ return [
         /*
          * Select default language
          */
-        'default' => 'en',
+        'default' => 'ru',
 
         /*
          * Select languages that are supported.
